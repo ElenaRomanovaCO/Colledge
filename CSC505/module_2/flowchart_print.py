09a66ba5afd93fbe16d3106cb9e6c5233a0853ae
ElenaRomanovaCO <eromanova115@gmail.com>
2024-06-05 21:41:58 -0600

    module 2 CSC505

---
+++ CSC505/module_2/flowchart_print.py
@@ -0,0 +1,18 @@
+"""
+This script will print flowchart steps.
+"""
+
+# Creating list with flowchat steps
+
+diagram_boxes = [
+    "Iterative Development",
+    "Continuous Feedback and Adaptation",
+    "Risk Management",
+    "Prototyping",
+    "Continuous Integration and Testing"
+]
+
+# iteration over created list of values to print each one consecutively
+
+for box in diagram_boxes:
+    print(box)
